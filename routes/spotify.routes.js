e9eae4079350f447bfb3c336124a5ee04c9815a5
Guglielmo Galasso <guglielmo.galasso1@gmail.com>
2023-05-15 15:40:19 +0100

    done

--- routes/spotify.routes.js
+++ routes/spotify.routes.js
@@ -15,42 +15,25 @@ router.get("/artist", async (req, res) => {
 	}
 });
 
-// going to albums of specific artist
-router.get("/albums/:artistId", async (req, res, next) => {
-  try {
-    let response = await spotifyApi.getArtistAlbums(req.params.artistId);
-
-    console.log(response.body.items);
-
-    res.render("artists/albums", { result: response.body.items });
-
-  } catch (error) {
-    console.log(error);
-  }
-});
-
-// going to tracks of specific album
-router.get("/album-tracks/:albumId", async (req, res) => {
-  try {
-    let response = await spotifyApi.getAlbumTracks(req.params.albumId);
-
-    //console.log(response.body.items);
-
-    res.render("artists/album-tracks", { result: response.body.items });
-
-  } catch (err) {
-    console.error("The error while searching album tracks occurred: ", err);
-  }
-});
-
-
 router.get("/album", async (req, res) => {
 	try {
 		let response = await spotifyApi.searchAlbums(req.query.characters);
-		//console.log(response.body.albums.items[0].artists)
+		//console.log(response.body.albums/* .items[0].artists */)
+
+      let sortedAlbums = response.body.albums.items
+      sortedAlbums.sort((a, b) => a.name.localeCompare(b.name))
+ 
+      let filteredAlbums = []
+      for (let i =0; i<sortedAlbums.length - 1; i++){
+          if (sortedAlbums[i].name !== sortedAlbums[i+1].name){
+              filteredAlbums.push(sortedAlbums[i])
+            }
+        }
+        
+        console.log("F: " + filteredAlbums.length, "A: " + response.body.albums.items.length)
 
 		res.render("artists/album-search.hbs", {
-			result: response.body.albums.items,
+			result: filteredAlbums /* response.body.albums.items */,
 		});
 	} catch (error) {
 		console.error(error);
@@ -69,5 +52,35 @@ router.get("/song", async (req, res) => {
 		console.error(error);
 	}
 });
+
+// going to albums of specific artist
+router.get("/albums/:artistId", async (req, res, next) => {
+    try {
+      let response = await spotifyApi.getArtistAlbums(req.params.artistId);
+  
+      //console.log(response.body.items);
+
+    
+  
+      res.render("artists/albums", { result: response.body.items });
+  
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  
+  // going to tracks of specific album
+  router.get("/album-tracks/:albumId", async (req, res) => {
+    try {
+      let response = await spotifyApi.getAlbumTracks(req.params.albumId);
+      //console.log(response.body);
+
+
+      res.render("artists/album-tracks", { result: filteredAlbums });
+  
+    } catch (err) {
+      console.error("The error while searching album tracks occurred: ", err);
+    }
+  });
 
 module.exports = router;
